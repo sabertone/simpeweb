01c836c613838ee45efe850c9d5afdf5b3885fb4
Santiago Bertone <ado.bertone@gmail.com>
2021-09-22 15:07:13 -0300

    change to adios

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = "0.0.0.0"
 
 // App
 const app = express()
-app.get("/", (req, res) => res.send("Hello World!"))
+app.get("/", (req, res) => res.send("Adios Mundo Cruel!"))
 
 app.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
